56973882757379bac8696e475472bee9a8befde6
Mostafizur <mostafizurdh2000@gmail.com>
2023-09-09 23:45:05 +0600

    device blog

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+
+$(document).ready(function () {
+    $(".banner").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        items: 1,
+        nav: false,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+    });
+    $(".latest_device_carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 4,
+            }
+        },
+        nav: true,
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+    });
+    $(".popular_mobile_carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        },
+        nav: true,
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+    });
+    
+
+});  
